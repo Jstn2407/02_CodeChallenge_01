b3f3626bbc8f347cc4890b6ed4ed24a57be2f085
Peter <schmidt.multimedia@gmail.com>
2021-02-08 15:13:06 +0100

    Variante 1 / Typkonvertierung

--- challenge_01.js
+++ challenge_01.js
@@ -11,9 +11,23 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 
 /*********** Variante 1  ****************/
 
+// Deklaration
+let ziffer1, ziffer2;
 let zahl1, zahl2, summe;
-zahl1 = 2;
-zahl2 = 2;
+
+// Eingabe :: Ziffern
+ziffer1 = prompt("Bitte Zahl 1 eingeben"); // 2
+ziffer2 = prompt("Bitte Zahl 2 eingeben"); // 2
+console.log(typeof ziffer1);
+console.log(typeof ziffer2);
+
+// Typkonvertierung | Ziffer --> Zahl (string --> number)
+zahl1 = parseInt(ziffer1);
+zahl2 = parseInt(ziffer2);
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+// Berechnung :: Summe
 summe = zahl1 + zahl2; 
 console.log("Die Summe ist: " + summe);
 
