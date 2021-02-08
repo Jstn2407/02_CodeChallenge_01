383e8af7ce936145f1f1421a298d05e32d4180d7
Peter <schmidt.multimedia@gmail.com>
2021-02-08 14:34:05 +0100

    Variante 1 / 1 (2+2)

--- challenge_01.js
+++ challenge_01.js
@@ -2,10 +2,18 @@
 /*********   CODE CHALLENGE 1 **********/
  
 /*
-Geben Sie über eine Promt-Anforderung eine Zahl ein
-Geben Sie über eine Promt-Anforderung eine zweite Zahl ein
+Geben Sie über eine Prompt-Anforderung eine Zahl ein
+Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein
 Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
+
+/*********** Variante 1  ****************/
+
+let zahl1, zahl2, summe;
+zahl1 = 2;
+zahl2 = 2;
+summe = zahl1 + zahl2; 
+console.log("Die Summe ist: " + summe);
 
