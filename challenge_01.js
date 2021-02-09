e1ce104ba0a12930496f427c6fa956175f68f15c
Justin <justinpessel@gmail.com>
2021-02-09 08:50:25 +0100

    Varinate 2 und 3

--- challenge_01.js
+++ challenge_01.js
@@ -10,7 +10,8 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 /*********** Variante 1  ****************/
-// zu viele Variablen
+/*
+// Vorteil: gute lesbarkeit | zu viele Variablen
 // Deklaration
 let ziffer1, ziffer2;
 let zahl1, zahl2, summe;
@@ -30,4 +31,26 @@ console.log(typeof zahl2);
 // Berechnung :: Summe
 summe = zahl1 + zahl2; 
 console.log("Die Summe ist: " + summe);
+*/
 
+
+/*********** Variante 2  ****************/
+/*
+// Kompaktere Lösung
+let zahl1, zahl2, summe;
+zahl1 = parseInt (prompt("Bitte Zahl 1 eingeben"));
+zahl2 = parseInt (prompt("Bitte Zahl 2 eingeben"));
+summe = zahl1 + zahl2;
+console.log("Die Summe ist:" + summe);
+*/
+
+/*********** Variante 3  ****************/
+//Kompakt, aber schlecht lesbar
+console.log
+(
+    "Die Summe ist: " + 
+    (
+        parseInt(prompt("Bitte Zahl1 eingeben"))
+        parseInt(prompt("Bitte Zahl2 eingeben"))
+    )
+);
